e399a308081f0d78d6d5208f3207f51ba566ce3c
Jan <jan@nedralia.se>
2025-08-11 12:02:56 +0200

    Import CSS

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 import ParallaxImage from "./ParallaxImage";
+import "./index.css";
+
+/**
+ * @module ParallaxImage
+ * @description A simple React component to display a parallax image.
+ * @see {@link https://github.com/iSadist/ParallaxImage}
+ */
 export default ParallaxImage;
